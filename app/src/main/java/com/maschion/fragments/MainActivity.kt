65242a1131f27f4bd0554a7e2092975fda7390a7
camila <cm.maschion@gmail.com>
2023-02-03 09:53:42 +0000

    actionbar activity

--- app/src/main/java/com/maschion/fragments/MainActivity.kt
+++ app/src/main/java/com/maschion/fragments/MainActivity.kt
@@ -2,12 +2,11 @@ package com.maschion.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.core.view.MenuProvider
 import com.maschion.fragments.databinding.ActivityMainBinding
-import com.maschion.fragments.fragments.HomeFragment
-import com.maschion.fragments.fragments.SecondFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,39 +18,47 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.home.setOnClickListener {
-
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<HomeFragment>(binding.fragmentContainerView.id)
-                addToBackStack(null)
-            }
-
-        }
-
-        binding.secondButton.setOnClickListener {
-
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<SecondFragment>(binding.fragmentContainerView.id)
-                addToBackStack(null)
-            }
-
-        }
-
-            //pilha de retorno
-
-        binding.showLogs.setOnClickListener {
-            showBackStackFragments()
-
-        }
+        setupActionBar()
 
     }
 
-    private fun showBackStackFragments() {
-        Log.i("Teste", "##########")
-        supportFragmentManager.fragments.forEach {
-            Log.i("Teste", it.toString())
-        }
+    private fun setupActionBar() {
+        addMenuProvider(object : MenuProvider{
+
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_home, menu)
+            }
+
+            //na seleção do item do menu
+            //toda vez que clicar em qualquer item do menu vai chamar essa função
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+
+                when(menuItem.itemId){
+
+                    R.id.action_save -> {
+//                        save()
+                    }
+
+                    R.id.action_config -> {
+                        menuItem.isChecked = !menuItem.isChecked
+//                        openSetting()
+
+                    }
+
+                }
+
+                return true
+            }
+
+        })
     }
+
+//    private fun openSetting() {
+//        TODO("Not yet implemented")
+//    }
+//
+//    private fun save() {
+//        TODO("Not yet implemented")
+//    }
+
 }
